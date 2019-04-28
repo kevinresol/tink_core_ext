02c2fd26b929f035c09922c7886ab9f621763b27
Kevin Leung <kevinresol@gmail.com>
2019-04-28 22:14:31 +0800

    Approach it OOly

--- src/tink/core/ext/Promises.hx
+++ src/tink/core/ext/Promises.hx
@@ -11,7 +11,7 @@ using tink.MacroApi;
 class Promises {
 	
 	public static macro function multi(e:Expr, ?lazy:Expr):Expr {
-		return switch Context.typeof(e) {
+		return switch haxe.macro.Context.typeof(e) {
 			case TAnonymous(_.get() => {fields: fields}):
 				
 				var obj:Array<Field> = [];
@@ -20,22 +20,19 @@ class Promises {
 				for(field in fields) {
 					var name = field.name;
 					
-					var ct = promiseType(field.type.toComplex());
-					switch ct {
-						case TPath({name: 'DirectType', pack: ['tink', 'macro']}): field.pos.error('Cannot determine the type of ${field.name}, please hint its type explicitly');
-						case _: // ok
+					switch promiseType(field.type.toComplex()) {
+						case TPath({name: 'DirectType', pack: ['tink', 'macro']}):
+							field.pos.error('Cannot determine the type of ${field.name}, please hint its type explicitly');
+						case ct: 
+							obj.push({
+								name: name,
+								kind: FVar(ct),
+								pos: field.pos,
+								meta: [{name: ':optional', pos: field.pos}],
+							});
+							
+							exprs.push(macro (__obj.$name:Promise<$ct>).handle(__ctx.handle(function(r, v) r.$name = v)));
 					}
-					obj.push({
-						name: name,
-						kind: FVar(ct),
-						pos: field.pos,
-						meta: [{name: ':optional', pos: field.pos}],
-					});
-					
-					exprs.push(
-						macro (__obj.$name:Promise<$ct>)
-							.handle(@:privateAccess tink.core.ext.Promises.handle(cb, function(v) __ctx.ret.$name = v, __ctx))
-					);
 				}
 				
 				switch lazy {
@@ -46,22 +43,14 @@ class Promises {
 				var ct = TAnonymous(obj);
 				return macro @:pos(e.pos) {
 					var __obj = $e;
-					var __ctx:{ret:$ct, count:Int} = {ret: {}, count: $v{fields.length}};
-					Promise.lift(Future.async(function(cb) $b{exprs}, $lazy));
+					Promise.lift(Future.async(function(cb) {
+						var __ctx = new tink.core.ext.Promises.Container<$ct>(cb, $v{fields.length});
+						$b{exprs}
+					}, $lazy));
 				}
 				
 			default:
 				e.pos.error('Expected inline object declaration');
-		}
-	}
-	
-	static function handle<T, R>(cb:Outcome<R, Error>->Void, assign:T->Void, ctx:{ret:R, count:Int}) {
-		return function(o) switch o {
-			case Success(v):
-				assign(v);
-				if(--ctx.count == 0) cb(Success(ctx.ret));
-			case Failure(e):
-				cb(Failure(e));
 		}
 	}
 	
@@ -73,4 +62,25 @@ class Promises {
 		}).toComplex();
 	}
 	#end
+}
+
+class Container<T:{}> {
+	var result:T = cast {};
+	var count:Int;
+	var cb:Outcome<T, Error>->Void;
+	
+	public function new(cb, count) {
+		this.cb = cb;
+		this.count = count;
+	}
+	
+	public function handle<R>(assign:T->R->Void) {
+		return function(o) switch o {
+			case Success(v):
+				assign(result, v);
+				if(--count == 0) cb(Success(result));
+			case Failure(e):
+				cb(Failure(e));
+		}
+	}
 }
